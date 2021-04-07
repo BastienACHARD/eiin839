9f74c7d7a0e8006a1d6f7c254488879cc48b28c6
BastienACHARD <43060957+BastienACHARD@users.noreply.github.com>
2021-04-07 14:59:50 +0200

    add

---
+++ TD5/TD Authentification Access Service toward SOAP/AuthentifiedAccess/ClientSOAP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.MathsOperationsClient calc = new ServiceReference1.MathsOperationsClient();
+            Console.WriteLine(calc.Add(1, 3));
+            Console.ReadLine();
+        }
+    }
+}
